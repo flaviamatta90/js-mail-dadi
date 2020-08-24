bc92d892fc6f90c76f34d6274664777d99ca88ae
Flavia <flaviamatta90@gmail.com>
2020-08-24 16:05:19 +0200

    finish

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,1 +1,16 @@
-alert("ciao");
+// Generare un numero random da 1  a 6, sia per il giocatore sia per il computer
+// 1 facciamo scegliere un numero al giocatore da 1 a 6
+var numeroGiocatore = Math.floor(Math.random() * 6) + 1;
+
+var numeroRandom = Math.floor(Math.random() * 6) + 1;
+
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+if(numeroGiocatore > numeroRandom){
+  console.log('Il tuo numero ' + numeroGiocatore + ' è il più alto! Hai Vinto!!')
+} else if(numeroRandom > numeroGiocatore)
+{
+  console.log('Il numero del computer ' + numeroRandom + ' è più alto del tuo! Hai Perso!!')
+} else
+{
+  console.log('I numeri sono uguali ' + numeroGiocatore + ' ' + numeroRandom);
+}
